cb96f636b14cdeda36a9947ec032803dbbe1a9b8
Jonatan Perret <perretjonatan1@gmail.com>
2024-10-14 16:21:34 +0200

    main class done

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -1,13 +1,16 @@
 package ch.heig.dai.lab.fileio;
 
 import java.io.File;
+import java.nio.charset.Charset;
 
-// *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.jehrensb.*;
+import ch.heig.dai.lab.fileio.JonatanPerret.EncodingSelector;
+import ch.heig.dai.lab.fileio.JonatanPerret.FileExplorer;
+import ch.heig.dai.lab.fileio.JonatanPerret.FileReaderWriter;
+import ch.heig.dai.lab.fileio.aleddaheig.Transformer;
 
 public class Main {
     // *** TODO: Change this to your own name ***
-    private static final String newName = "Jean-Claude Van Damme";
+    private static final String newName = "Jonatan Perret";
 
     /**
      * Main method to transform files in a folder.
@@ -33,9 +36,30 @@ public class Main {
         System.out.println("Application started, reading folder " + folder + "...");
         // TODO: implement the main method here
 
+        // Create the necessary objects
+        FileExplorer explorer = new FileExplorer(folder);
+        EncodingSelector selector = new EncodingSelector();
+        Transformer transformer = new Transformer(newName, wordsPerLine);
+        FileReaderWriter readerWriter = new FileReaderWriter();
+
         while (true) {
             try {
                 // TODO: loop over all files
+                File file = explorer.getNewFile();
+                if (file == null) {
+                    break;
+                }
+                System.out.println("Processing file " + file.getName() + "...");
+                // Determine the encoding
+                Charset encoding = selector.getEncoding(file);
+                // Read the file
+                String content = readerWriter.readFile(file, encoding);
+                // Transform the content
+                String transformedContent = transformer.replaceChuck(content);
+                transformedContent = transformer.capitalizeWords(transformedContent);
+                transformedContent = transformer.wrapAndNumberLines(transformedContent);
+                // print the result
+                System.out.println(transformedContent);
 
             } catch (Exception e) {
                 System.out.println("Exception: " + e);
